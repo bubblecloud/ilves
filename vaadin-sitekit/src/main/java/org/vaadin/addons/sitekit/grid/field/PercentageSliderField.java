1712784c17f1705183009a1c2498597011b8ed0e
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2014-01-09 21:44:01 +0200

    Implemented color picker and percentage slider fields.

---
+++ vaadin-sitekit/src/main/java/org/vaadin/addons/sitekit/grid/field/PercentageSliderField.java
@@ -0,0 +1,48 @@
+package org.vaadin.addons.sitekit.grid.field;
+
+import com.vaadin.data.Property;
+import com.vaadin.shared.ui.colorpicker.Color;
+import com.vaadin.ui.*;
+import com.vaadin.ui.components.colorpicker.ColorChangeEvent;
+import com.vaadin.ui.components.colorpicker.ColorChangeListener;
+
+/**
+ * Color picker form field.
+ */
+public class PercentageSliderField extends CustomField<Double> {
+
+    final Slider slider;
+
+    public PercentageSliderField() {
+        slider = new Slider();
+        slider.setMin(0);
+        slider.setMax(100);
+        slider.setSizeFull();
+        slider.addValueChangeListener(new ValueChangeListener() {
+            @Override
+            public void valueChange(Property.ValueChangeEvent event) {
+                setPercentageValue((Double) event.getProperty().getValue());
+            }
+        });
+    }
+
+    @Override
+    protected Component initContent() {
+        return slider;
+    }
+
+    protected void setPercentageValue(final Double value) {
+        super.setValue(value);
+    }
+
+    @Override
+    public Class<? extends Double> getType() {
+        return Double.class;
+    }
+
+    @Override
+    protected void setInternalValue(Double newValue) {
+        super.setInternalValue(newValue);
+        slider.setValue(newValue);
+    }
+}
