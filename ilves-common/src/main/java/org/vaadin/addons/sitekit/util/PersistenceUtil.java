688bfaaeb14c6a6e7615974507bbd95fcd9b4188
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2014-12-13 17:38:48 +0200

    Added helper method to easily generate liquibase change log entries according to JPA model changes.

--- ilves-common/src/main/java/org/vaadin/addons/sitekit/util/PersistenceUtil.java
+++ ilves-common/src/main/java/org/vaadin/addons/sitekit/util/PersistenceUtil.java
@@ -19,6 +19,12 @@ import liquibase.Liquibase;
 import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
+import liquibase.diff.DiffResult;
+import liquibase.diff.compare.CompareControl;
+import liquibase.diff.output.DiffOutputControl;
+import liquibase.diff.output.changelog.DiffToChangeLog;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
 import org.eclipse.persistence.config.PersistenceUnitProperties;
 import org.vaadin.addons.sitekit.site.SiteException;
@@ -26,6 +32,9 @@ import org.vaadin.addons.sitekit.site.SiteException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
@@ -112,5 +121,56 @@ public final class PersistenceUtil {
                 entityManagerFactories.remove(entityManagerFactoryKey);
             }
         }
+    }
+
+    /**
+     * Diffs database schema against new changes in JPA model.
+     *
+     * @param persistenceUnit the persistence unit
+     * @param propertiesCategory the properties category
+     * @return the entity manager factory singleton
+     */
+    public static String diff(final String persistenceUnit, final String propertiesCategory) throws Exception {
+
+        final String refJdbcUrl = PropertiesUtil.getProperty(
+                propertiesCategory, PersistenceUnitProperties.JDBC_URL) + "ref";
+
+        // Construct schema according to liquibase changelog.
+        PropertiesUtil.setProperty(propertiesCategory, PersistenceUnitProperties.DDL_GENERATION, "none");
+        final EntityManagerFactory factory = getEntityManagerFactory(persistenceUnit, propertiesCategory);
+
+        // Construct ref schema according to liquibase changelog.
+        PropertiesUtil.setProperty(propertiesCategory, PersistenceUnitProperties.JDBC_URL, refJdbcUrl);
+        getEntityManagerFactory(persistenceUnit, propertiesCategory);
+
+        // Update ref schema according to JPA changes.
+        PropertiesUtil.setProperty(propertiesCategory, PersistenceUnitProperties.DDL_GENERATION, "create-and-extend-tables");
+        PropertiesUtil.setProperty(propertiesCategory, PersistenceUnitProperties.JDBC_URL, refJdbcUrl);
+        final EntityManagerFactory refFactory = getEntityManagerFactory(persistenceUnit, propertiesCategory);
+
+
+        final String changeLog = PropertiesUtil.getProperty(
+                propertiesCategory, "liquibase-change-log");
+
+        final EntityManager entityManager = factory.createEntityManager();
+        final EntityManager refEntityManager = refFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        refEntityManager.getTransaction().begin();
+        final Connection connection = entityManager.unwrap(Connection.class);
+        final Connection refConnection = refEntityManager.unwrap(Connection.class);
+        final Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+        final Database refDatabase = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(refConnection));
+        final Liquibase liquibase = new Liquibase(changeLog, new ClassLoaderResourceAccessor(), database);
+        final DiffResult diffResult = liquibase.diff(refDatabase, database, CompareControl.STANDARD);
+
+        final ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+        final PrintStream printStream=new PrintStream(byteArrayOutputStream);
+        final DiffToChangeLog diffToChangeLog = new DiffToChangeLog(diffResult, new DiffOutputControl());
+        diffToChangeLog.print(printStream);
+
+        entityManager.getTransaction().rollback();
+        refEntityManager.getTransaction().rollback();
+
+        return byteArrayOutputStream.toString();
     }
 }
